c7f8c093e902c762dc2ca02de120e7de44788986
KenNyt333 <163571624+KenNyt333@users.noreply.github.com>
2026-04-16 12:53:58 +0530

    Update Timeline.tsx

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,16 +2,16 @@ import { motion } from 'framer-motion';
 import { Heart, Plane, Camera, Coffee, Star, Gift, Home, Sparkles } from 'lucide-react';
 
 const milestones = [
-  { date: 'December 28 2024', event: 'The day we first texted', icon: Heart, color: 'from-pink-400 to-rose-400' },
-  { date: 'Propose Day - ', event: 'We Break our silence ', icon: Coffee, color: 'from-purple-400 to-pink-400' },
+  { date: 'December 28 2024', event: 'The day we first texted', icon: Sparkles, color: 'from-blue-400 to-purple-400' },
+  { date: 'Propose Day - ', event: 'We Break our silence ', icon: Heart, color: 'from-pink-400 to-rose-400' },
   { date: 'January 1 2026', event: 'We planned our next months all night (but didnt done it😂)', icon: Sparkles, color: 'from-blue-400 to-purple-400' },
-  { date: 'First Meet - April 1 2026', event: 'Came to your college, saw you first time MY LOVE, picked you up felt butterflies that i didnt know how to speak ', icon: Plane, color: 'from-teal-400 to-blue-400' },
-  { date: 'First touch and kiss - April 1 2026', event: 'Saw my mamiyar and mamanar(fierce) first time. Our First kiss, First Hug in your kitchen', icon: Star, color: 'from-green-400 to-teal-400' },
-  { date: 'April 2 2026', event: 'First time church with you(but i didnt see you)', icon: Camera, color: 'from-yellow-400 to-green-400' },
-  { date: 'April 3 2026', event: 'Spend long night with you, noon gone to bhai and kissed your cheek', icon: Home, color: 'from-orange-400 to-yellow-400' },
-  { date: 'April 5 2026', event: ' Church lo same bench and a cute lil pic', icon: Gift, color: 'from-red-400 to-orange-400' },
+  { date: 'First Meet - April 1 2026', event: 'Came to your college, saw you first time MY LOVE, picked you up felt butterflies that i didnt know how to speak ', icon: Sparkles, color: 'from-blue-400 to-purple-400' },
+  { date: 'First touch and kiss - April 1 2026', event: 'Saw my mamiyar and mamanar(fierce) first time. Our First kiss, First Hug in your kitchen', icon: Sparkles, color: 'from-blue-400 to-purple-400' },
+  { date: 'April 2 2026', event: 'First time church with you(but i didnt see you)', icon: Star, color: 'from-green-400 to-teal-400' },
+  { date: 'April 3 2026', event: 'Spend long night with you, noon gone to bhai and kissed your cheek', icon: Sparkles, color: 'from-blue-400 to-purple-400' },
+  { date: 'April 5 2026', event: ' Church lo same bench and a cute lil pic', icon: Camera, color: 'from-yellow-400 to-green-400' },
   { date: 'July 6 2026', event: 'First Movie together, RAAKASAA👹😂', icon: Camera, color: 'from-yellow-400 to-green-400' }
-  { date: 'July 7 2026', event: 'Said our first goodbyes with lots of memories', icon: Camera, color: 'from-yellow-400 to-green-400' }
+  { date: 'July 7 2026', event: 'Said our first goodbyes with lots of memories', icon: Heart, color: 'from-pink-400 to-rose-400' }
 ];
 
 export default function Timeline() {
